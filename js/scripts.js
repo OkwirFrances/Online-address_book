a5c9d90fb08c1d56eef085422d200fd78e8b8aa5
okwir <francisokwir24@gmail.com>
2019-03-29 10:42:48 +0300

    still modifying this

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('#form-button').click(function(){
+
+    $(".container").hide();
+    $(".container2").show();
+    $(".info").on('submit', function(event){
+      event.preventDefault();
+    var firstName = $("#fname").val();
+     var lastName = $("input#lname").val();
+     var contact1 ={firstname:firstName, lastname:lastName};
+     console.log(contact1);
+     $("#names ").append("<li>"+contact1.firstname+" "+contact1.lastname+"</li>");
+       $(".container").show();
+       $(".container2").fadeOut();
+       
+     })
+  })
+})
